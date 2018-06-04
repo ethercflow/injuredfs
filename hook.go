7dcaf787589b4cfe73b86243ab72cc9e1aeb5c90
Zwb <ethercflow@gmail.com>
2018-06-04 15:28:25 +0800

    feat(hook): init InjuredHook

---
+++ hook.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	hookfs "github.com/osrg/hookfs/hookfs"
+)
+
+// implements hookfs.HookContext
+type InjuredHookContext struct {
+	path string
+}
+
+// implements hookfs.Hook
+type InjuredHook struct{}
+
+// implements hookfs.HookWithInit
+func (this *InjuredHook) Init() error {
+	log.WithFields(log.Fields{
+		"this": this,
+	}).Info("InjuredHook Init: initializing")
+	return nil
+}
+
+// implements hookfs.HookOnOpen
+func (this *InjuredHook) PreOpen(path string, flags uint32) (error, bool, hookfs.HookContext) {
+	ctx := InjuredHookContext{path: path}
+	return nil, false, ctx
+}
+
+// implements hookfs.HookOnOpen
+func (this *InjuredHook) PostOpen(realRetCode int32, ctx hookfs.HookContext) (error, bool) {
+	return nil, false
+}
+
+// implements hookfs.HookOnRead
+func (this *InjuredHook) PreRead(path string, length int64, offset int64) ([]byte, error, bool, hookfs.HookContext) {
+	ctx := InjuredHookContext{path: path}
+	sleep := 3 * time.Second
+	log.WithFields(log.Fields{
+		"this":  this,
+		"ctx":   ctx,
+		"sleep": sleep,
+	}).Info("Injured PreRead: sleeping")
+	time.Sleep(sleep)
+	return nil, nil, false, ctx
+}
+
+// implements hookfs.HookOnRead
+func (this *InjuredHook) PostRead(realRetCode int32, realBuf []byte, ctx hookfs.HookContext) ([]byte, error, bool) {
+	return nil, nil, false
+}
+
+// implements hookfs.HookOnMkdir
+func (this *InjuredHook) PreMkdir(path string, mode uint32) (error, bool, hookfs.HookContext) {
+	ctx := InjuredHookContext{path: path}
+	return nil, false, ctx
+}
+
+// implements hookfs.HookOnMkdir
+func (this *InjuredHook) PostMkdir(realRetCode int32, ctx hookfs.HookContext) (error, bool) {
+	return nil, false
+}
+
+// implements hookfs.HookOnRmdir
+func (this *InjuredHook) PreRmdir(path string) (error, bool, hookfs.HookContext) {
+	ctx := InjuredHookContext{path: path}
+	return nil, false, ctx
+}
+
+// implements hookfs.HookOnRmdir
+func (this *InjuredHook) PostRmdir(realRetCode int32, ctx hookfs.HookContext) (error, bool) {
+	return nil, false
+}
+
+// implements hookfs.HookOnOpenDir
+func (this *InjuredHook) PreOpenDir(path string) (error, bool, hookfs.HookContext) {
+	ctx := InjuredHookContext{path: path}
+	return nil, false, ctx
+}
+
+// implements hookfs.HookOnOpenDir
+func (this *InjuredHook) PostOpenDir(realRetCode int32, ctx hookfs.HookContext) (error, bool) {
+	return nil, false
+}
+
+// implements hookfs.HookOnFsync
+func (this *InjuredHook) PreFsync(path string, flags uint32) (error, bool, hookfs.HookContext) {
+	ctx := InjuredHookContext{path: path}
+	if path != "" {
+		sleep := 3 * time.Second
+		log.WithFields(log.Fields{
+			"this":  this,
+			"ctx":   ctx,
+			"sleep": sleep,
+		}).Info("Injured PreFsync: sleeping")
+		time.Sleep(sleep)
+	}
+	return nil, false, ctx
+}
+
+// implements hookfs.HookOnFsync
+func (this *InjuredHook) PostFsync(realRetCode int32, ctx hookfs.HookContext) (error, bool) {
+	return nil, false
+}
