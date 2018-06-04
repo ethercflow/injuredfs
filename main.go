2bc33aba77e4377134b5354cf411821c47ac29e5
Zwb <ethercflow@gmail.com>
2018-06-04 18:09:11 +0800

    feat(main): setup http server and fs server

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
+
+	"github.com/osrg/hookfs/hookfs"
 )
 
-var globalInjuredHook *InjuredHook
+var (
+	globalInjuredHook *InjuredHook
+
+	addr       = flag.String("addr", ":65534", "The address to bind to")
+	original   = flag.String("original", "", "ORIGINAL")
+	mountpoint = flag.String("mountpoint", "", "MOUNTPOINT")
+	logLevel   = flag.Int("log-level", 0, fmt.Sprintf("log level (%d..%d)", hookfs.LogLevelMin, hookfs.LogLevelMax))
+)
 
 func main() {
 	sc := make(chan os.Signal, 1)
@@ -32,4 +44,30 @@ func main() {
 			os.Exit(1)
 		}
 	}()
+
+	flag.Parse()
+	if *original == "" || *mountpoint == "" {
+		os.Exit(2)
+	}
+
+	hookfs.SetLogLevel(*logLevel)
+	globalInjuredHook = &InjuredHook{
+		read:  make(map[string]time.Duration),
+		rl:    &sync.RWMutex{},
+		fsync: make(map[string]time.Duration),
+		fl:    &sync.RWMutex{},
+	}
+	fs, err := hookfs.NewHookFs(*original, *mountpoint, globalInjuredHook)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := &Server{}
+	go func() {
+		log.Fatal(server.Run(*addr))
+	}()
+
+	if err = fs.Serve(); err != nil {
+		log.Fatal(err)
+	}
 }
