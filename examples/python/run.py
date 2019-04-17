7ca67732793ab032eba92378febf5efded17c6b4
Zwb <ethercflow@gmail.com>
2019-04-17 14:38:27 +0800

    add python example

---
+++ examples/python/run.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import grpc
+import injure_pb2
+import injure_pb2_grpc
+from datetime import datetime
+
+if __name__ == "__main__":
+    methods = "write"
+    errno = 0
+    random = False
+    pct = 100
+    path = "testdir/*"
+    delay = 5000000
+
+    print "fault..."
+
+    with grpc.insecure_channel('127.0.0.1:65534') as channel:
+        stub = injure_pb2_grpc.InjureStub(channel)
+        stub.SetFault(injure_pb2.Request(methods=methods.split(
+            ","), errno=errno, random=random, pct=pct, path=path, delay=delay))
+
+    b = datetime.now()
+    with open('/mnt/edata/testdir/output.txt', 'w') as f:
+        f.write('Hi there!')
+    e = datetime.now()
+
+    print "write cost %d seconds, do recover..." % (e - b).seconds
+
+    with grpc.insecure_channel('127.0.0.1:65534') as channel:
+        stub = injure_pb2_grpc.InjureStub(channel)
+        stub.RecoverMethod(injure_pb2.Request(methods=methods.split(",")))
+
+    b = datetime.now()
+    with open('/mnt/edata/testdir/output.txt', 'w') as f:
+        f.write('Hi there!')
+    e = datetime.now()
+    print "write cost %d seconds after recover..." % (e - b).seconds
