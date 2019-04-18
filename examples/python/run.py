e4f70e5fe8bb43acdade73f22ac017a2812a97c8
Zwb <ethercflow@gmail.com>
2019-04-18 15:38:37 +0800

    update python example

--- examples/python/run.py
+++ examples/python/run.py
@@ -36,3 +36,19 @@ if __name__ == "__main__":
         f.write('Hi there!')
     e = datetime.now()
     print "write cost %d seconds after recover..." % (e - b).seconds
+
+    errno = 0x1c  # NOSPC
+    with grpc.insecure_channel('127.0.0.1:65534') as channel:
+        stub = injure_pb2_grpc.InjureStub(channel)
+        stub.SetFault(injure_pb2.Request(methods=methods.split(
+            ","), errno=errno, random=random, pct=pct, path=path, delay=0))
+
+    try:
+        with open('/mnt/edata/testdir/output.txt', 'w') as f:
+            f.write('Hi there!')
+    except IOError as e:
+        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+    finally:
+        with grpc.insecure_channel('127.0.0.1:65534') as channel:
+            stub = injure_pb2_grpc.InjureStub(channel)
+            stub.RecoverMethod(injure_pb2.Request(methods=methods.split(",")))
